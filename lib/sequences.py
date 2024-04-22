658e60beeac4ee5dfa6a7d024d61789b6e50a2e8
Hollis Crowder <hollis.crowder@gmail.com>
2024-04-22 13:17:37 -0700

    complete sequences lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
+
 def print_fibonacci(length):
-    pass
+    fibonacci = []
+    if length == 0:
+        fibonacci = []
+    elif length == 1:
+        fibonacci = [0]
+    elif length == 2:
+        fibonacci = [0, 1]
+    elif length > 2:
+        fibonacci = [0, 1]
+        i = 2
+        while i in range(length):
+            fibonacci.append(fibonacci[-1] + fibonacci[-2])
+            i += 1
+
+    print(fibonacci)
